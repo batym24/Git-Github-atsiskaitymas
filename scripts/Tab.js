6757af45fc5d23287294aacc791b59c4aa904c28
batym24 <baty.mantas@gmail.com>
2023-04-18 13:30:45 +0300

    added video resource and styling to the tab

--- scripts/Tab.js
+++ scripts/Tab.js
@@ -13,12 +13,20 @@ export default class Tab {
         this.introText = document.createTextNode("INTRO")
         this.intro.appendChild(this.introText)
 
+        this.video = document.createElement('video')
+        this.video.setAttribute("width", "90%")
+        this.video.setAttribute("controls", "")
+        this.videoSource = document.createElement('source')
+        this.videoSource.setAttribute("src", "./resources/tab-1.mp4")
+        this.videoSource.setAttribute('type', "video/mp4")
+        this.video.appendChild(this.videoSource)
+
         this.closeImage = document.createElement('img')
         this.closeImage.setAttribute("src", "./resources/xmark-solid.svg")
         this.closeImage.classList.add("close-image")
         this.remove()
 
-        this.introDiv.append(this.intro, this.closeImage)
+        this.introDiv.append(this.intro, this.video, this.closeImage)
         return this.introDiv
     }
 

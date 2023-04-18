8e6f18d2b6694d557e2dde4149417b74db751cbf
batym24 <baty.mantas@gmail.com>
2023-04-18 13:40:54 +0300

    added text to tab

--- scripts/Tab.js
+++ scripts/Tab.js
@@ -14,19 +14,24 @@ export default class Tab {
         this.intro.appendChild(this.introText)
 
         this.video = document.createElement('video')
-        this.video.setAttribute("width", "90%")
         this.video.setAttribute("controls", "")
         this.videoSource = document.createElement('source')
         this.videoSource.setAttribute("src", "./resources/tab-1.mp4")
         this.videoSource.setAttribute('type', "video/mp4")
         this.video.appendChild(this.videoSource)
 
+        this.firstPar = document.createElement('p')
+        this.firstPar.appendChild(document.createTextNode("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."))
+
+        this.secondPar = document.createElement('p')
+        this.secondPar.appendChild(document.createTextNode("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."))
+
         this.closeImage = document.createElement('img')
         this.closeImage.setAttribute("src", "./resources/xmark-solid.svg")
         this.closeImage.classList.add("close-image")
         this.remove()
 
-        this.introDiv.append(this.intro, this.video, this.closeImage)
+        this.introDiv.append(this.intro, this.video, this.firstPar, this.secondPar, this.closeImage)
         return this.introDiv
     }
 
